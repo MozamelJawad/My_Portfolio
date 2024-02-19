f5dd81956392b7298ff12771c7cbb21b406dcd4b
Mozamel Jawad <mozameljawad@gmail.com>
2024-02-19 20:59:38 +0430

    add scroll reveal

--- script.js
+++ script.js
@@ -38,3 +38,23 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+
+// scroll reveal animation
+
+ScrollReveal({
+  // reset: true, 
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+// typed js 
+
+
