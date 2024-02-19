2c0595708d96d384bfa5f3ea113af4cd3484bb26
Mozamel Jawad <mozameljawad@gmail.com>
2024-02-19 20:58:28 +0430

    manage scroll sections active link and sticky navbar

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+
+// scroll sections active link and sticky navbar
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    // active link
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click on link 
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
